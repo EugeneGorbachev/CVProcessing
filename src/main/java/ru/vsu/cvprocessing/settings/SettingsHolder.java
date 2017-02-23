8a309eda7ca34dcbcf8cee768827cc23a316c0bc
EugeneGorbachev <aurantiaco@yandex.ru>
2017-02-23 16:50:44 +0300

    Similar class creation replaced with "ColorProperty" class.

--- src/main/java/ru/vsu/cvprocessing/settings/SettingsHolder.java
+++ src/main/java/ru/vsu/cvprocessing/settings/SettingsHolder.java
@@ -12,8 +12,6 @@ import ru.vsu.cvprocessing.recognition.ImageRecognition;
 import javafx.scene.paint.Color;
 
 public class SettingsHolder {
-    private static final Logger log = Logger.getLogger(SettingsHolder.class);
-
     public final static String FXML_FILE_PREF = "../../../../fxml/";
 
     /* Singleton */
@@ -61,66 +59,9 @@ public class SettingsHolder {
         this.cameraHolder = cameraHolder;
         this.imageRecognition = imageRecognition;
         this.camera = camera;
-        this.markerColorProperty = new ObjectPropertyBase<Color>(markerColor) {
-            @Override
-            public void set(Color newValue) {
-                Color oldValue = get();
-                super.set(newValue);
-                log.info(String.format("%s value was changed from ", getName()) +
-                        String.format("H:%1.2f S:%1.2f B:%1.2f", oldValue.getHue(), oldValue.getSaturation(), oldValue.getBrightness()) +
-                        String.format(" to H:%1.2f S:%1.2f B:%1.2f", newValue.getHue(), newValue.getSaturation(), newValue.getBrightness()));
-            }
-
-            @Override
-            public Object getBean() {
-                return this;
-            }
-
-            @Override
-            public String getName() {
-                return "Marker color";
-            }
-        };
-        this.colorRangeStartProperty = new ObjectPropertyBase<Color>(colorRangeStart) {
-            @Override
-            public void set(Color newValue) {
-                Color oldValue = get();
-                super.set(newValue);
-                log.info(String.format("%s value was changed from ", getName()) +
-                        String.format("H:%1.2f S:%1.2f B:%1.2f", oldValue.getHue(), oldValue.getSaturation(), oldValue.getBrightness()) +
-                        String.format(" to H:%1.2f S:%1.2f B:%1.2f", newValue.getHue(), newValue.getSaturation(), newValue.getBrightness()));
-            }
-
-            @Override
-            public Object getBean() {
-                return this;
-            }
-
-            @Override
-            public String getName() {
-                return "Image recognition color range start";
-            }
-        };
-        this.colorRangeEndProperty = new ObjectPropertyBase<Color>(colorRangeEnd) {
-            @Override
-            public void set(Color newValue) {
-                Color oldValue = get();
-                super.set(newValue);
-                log.info(String.format("%s color was changed from ", getName()) +
-                        String.format("H:%1.2f S:%1.2f B:%1.2f", oldValue.getHue(), oldValue.getSaturation(), oldValue.getBrightness()) +
-                        String.format(" to H:%1.2f S:%1.2f B:%1.2f", newValue.getHue(), newValue.getSaturation(), newValue.getBrightness()));
-            }
-
-            @Override
-            public Object getBean() {
-                return this;
-            }
-
-            @Override
-            public String getName() {
-                return "Image recognition color range end";
-            }
-        };
+        this.markerColorProperty = new ColorProperty(markerColor, "Marker color");
+        this.colorRangeStartProperty = new ColorProperty(colorRangeStart, "Image recognition color range start");
+        this.colorRangeEndProperty = new ColorProperty(colorRangeEnd, "Image recognition color range end");
         this.haarCascadeConfigFilename = haarCascadeConfigFilename;
     }
 
@@ -193,4 +134,34 @@ public class SettingsHolder {
         this.haarCascadeConfigFilename = haarCascadeConfigFilename;
     }
     /* Getters and setters */
+}
+
+class ColorProperty extends ObjectPropertyBase<Color> {
+    private static final Logger log = Logger.getLogger(ColorProperty.class);
+
+    private String propertyName;
+
+    public ColorProperty(Color initialValue, String propertyName) {
+        super(initialValue);
+        this.propertyName = propertyName;
+    }
+
+    @Override
+    public void set(Color newValue) {
+        Color oldValue = get();
+        super.set(newValue);
+        log.info(String.format("%s value was changed from ", getName()) +
+                String.format("H:%1.2f S:%1.2f B:%1.2f", oldValue.getHue(), oldValue.getSaturation(), oldValue.getBrightness()) +
+                String.format(" to H:%1.2f S:%1.2f B:%1.2f", newValue.getHue(), newValue.getSaturation(), newValue.getBrightness()));
+    }
+
+    @Override
+    public Object getBean() {
+        return this;
+    }
+
+    @Override
+    public String getName() {
+        return propertyName;
+    }
 }
