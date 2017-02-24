3c71e70fbb47b8762e2d692a5b89cc6449dad76a
314Bravo <Ilyatmb7@rambler.ru>
2017-02-24 13:26:55 +0300

    JoyTest

--- src/main/resources/arduinocode/JoyTest/JoyTest.ino
+++ src/main/resources/arduinocode/JoyTest/JoyTest.ino
@@ -1,17 +1,32 @@
 #include <Servo.h>
-int joyX=0; //Положение джостика
-int angl=0; //Угол серво
-Servo horizontalServo;
+int joyX=0; //analog A0
+int joyY=0; //analog A1
+int btnState=0; // knob A3
+int pos=0; //Угол серво
+Servo horizontalServo,verticalServo;
 void setup() {
-horizontalServo.attach(11);
+horizontalServo.attach(8);
+verticalServo.attach(9);
 pinMode(joyX,INPUT);
+pinMode(joyY,INPUT);
+pinMode(btn,INPUT);
+
 Serial.begin(9600);
 }
 void loop() {
-  angl = analogRead(joyX); //Считываем аналоговый вход (влево 0 вправо 1023)
- 
-  angl = map(angl,0,1023,0,180);
-  horizontalServo.write(angl);
+analogRead(KnobZ);
+  if (KnobZ>0)
+  {
+  
+   
+  pos = analogRead(joyX); //Считываем аналоговый вход (влево 0 вправо 1023)
+  pos = map(pos,0,1023,0,180);
+  verticalServo.write(180-pos);
+  
+  pos = analogRead(joyY);
+  pos = map(pos,0,1023,0,180);
+  horizontalServo.write(pos);
+  }
   
 }
     
